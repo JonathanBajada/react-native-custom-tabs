6ad84bf48dd8879c7cc8334294a1119690e74951
Jonatahn Bajada <jbajada@ryerson.ca>
2020-12-22 10:16:31 -0500

    css done for nav bar

--- components/Tab.tsx
+++ components/Tab.tsx
@@ -1,12 +1,27 @@
 import * as React from 'react';
 import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
+import { useState } from 'react';
 
 const Tab = ({ color, tab, onPress, icon }) => {
+	const [ selected, setSelected ] = useState('');
+	const handleStyle = (name: any) => {
+		if (name === 'Treatment') {
+			return styles.treatment;
+		}
+		if (name === 'Exercises') {
+			return styles.exercises;
+		}
+		if (name === 'Resources') {
+			return styles.resources;
+		}
+		return styles.icon;
+	};
+
 	return (
 		<TouchableOpacity onPress={onPress} style={styles.container}>
-			<Image style={styles.icon} source={icon} />
-			<Text style={{ color }}>{tab.name}</Text>
+			<Image style={handleStyle(tab.name)} source={icon} />
+			<Text style={{ color }}>{tab.name === 'Treatment' ? '' : tab.name}</Text>
 		</TouchableOpacity>
 	);
 };
@@ -22,5 +37,18 @@ const styles = StyleSheet.create({
 	icon: {
 		height: 24,
 		width: 20
+	},
+	treatment: {
+		height: 70,
+		width: 70,
+		marginBottom: 50
+	},
+	exercises: {
+		height: 24,
+		width: 30
+	},
+	resources: {
+		height: 24,
+		width: 25
 	}
 });
--- components/TabNavigator.tsx
+++ components/TabNavigator.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Home from '../screens/Home';
+import Clients from '../screens/Clients';
 import Profile from '../screens/Profile';
-import Create from '../screens/Create';
+import Exercises from '../screens/Exercises';
 import Tabbar from '../components/Tabbar';
-import Client from '../screens/Client';
+import Resources from '../screens/Resources';
 import Treatment from '../screens/Treatment';
 
 import ClientIMG from '../images/Clients.png';
@@ -15,19 +15,16 @@ import ToolsIMG from '../images/ToolsIcon.png';
 import ProfileIconIMG from '../images/ProfileIcon.png';
 
 const Tab = createBottomTabNavigator();
-
 const TabNavigator = () => {
 	return (
 		<Tab.Navigator tabBar={(props) => <Tabbar {...props} />}>
-			<Tab.Screen name="Home" component={Home} initialParams={{ icon: ClientIMG }} />
-			<Tab.Screen name="Create" component={Create} initialParams={{ icon: ExerciseIMG }} />
-			<Tab.Screen name="Profile" component={Profile} initialParams={{ icon: TreatmentIMG }} />
-			<Tab.Screen name="Client" component={Client} initialParams={{ icon: ToolsIMG }} />
-			<Tab.Screen name="Treatment" component={Treatment} initialParams={{ icon: ProfileIconIMG }} />
+			<Tab.Screen name="Profile" component={Profile} initialParams={{ icon: ProfileIconIMG }} />
+			<Tab.Screen name="Exercises" component={Exercises} initialParams={{ icon: ExerciseIMG }} />
+			<Tab.Screen name="Treatment" component={Treatment} initialParams={{ icon: TreatmentIMG }} />
+			<Tab.Screen name="Clients" component={Clients} initialParams={{ icon: ClientIMG }} />
+			<Tab.Screen name="Resources" component={Resources} initialParams={{ icon: ToolsIMG }} />
 		</Tab.Navigator>
 	);
 };
-
-const styles = StyleSheet.create({});
 
 export default TabNavigator;
